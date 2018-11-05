d4363e412e27703d34f6128b56cf7198b92a0413
Yigit Ersun <yigitersun@hotmail.com>
2018-11-05 17:17:42 +0300

    Application runnable on linux.

---
+++ linuxservicehostconsole/ApplicationLifetimeHostedService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace linuxservicehostconsole
+{
+    public class ApplicationLifetimeHostedService : IHostedService
+    {
+        IApplicationLifetime appLifetime;
+        ILogger<ApplicationLifetimeHostedService> logger;
+        IHostingEnvironment environment;
+        IConfiguration configuration;
+        public ApplicationLifetimeHostedService(
+            IConfiguration configuration,
+            IHostingEnvironment environment,
+            ILogger<ApplicationLifetimeHostedService> logger,
+            IApplicationLifetime appLifetime)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+            this.appLifetime = appLifetime;
+            this.environment = environment;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("StartAsync method called.");
+
+            this.appLifetime.ApplicationStarted.Register(OnStarted);
+            this.appLifetime.ApplicationStopping.Register(OnStopping);
+            this.appLifetime.ApplicationStopped.Register(OnStopped);
+
+            return Task.CompletedTask;
+
+        }
+
+        private void OnStarted()
+        {
+            this.logger.LogInformation("OnStarted method called.");
+
+            // Post-startup code goes here  
+        }
+
+        private void OnStopping()
+        {
+            this.logger.LogInformation("OnStopping method called.");
+
+            // On-stopping code goes here  
+        }
+
+        private void OnStopped()
+        {
+            this.logger.LogInformation("OnStopped method called.");
+
+            // Post-stopped code goes here  
+        }
+
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("StopAsync method called.");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ linuxservicehostconsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace linuxservicehostconsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = new HostBuilder()
+                 .ConfigureHostConfiguration(configHost =>
+                 {
+                     configHost.SetBasePath(Directory.GetCurrentDirectory());
+                     configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                     configHost.AddCommandLine(args);
+                 })
+                 .ConfigureAppConfiguration((hostContext, configApp) =>
+                 {
+                     configApp.SetBasePath(Directory.GetCurrentDirectory());
+                     configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                     configApp.AddJsonFile($"appsettings.json", true);
+                     configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+                     configApp.AddCommandLine(args);
+                 })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddHostedService<ApplicationLifetimeHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddSerilog(new LoggerConfiguration()
+                              .ReadFrom.Configuration(hostContext.Configuration)
+                              .CreateLogger());
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
